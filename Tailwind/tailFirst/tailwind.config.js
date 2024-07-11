a3e27b9f3f9428be10c245b1becb82acd775a35d
Ersin Mutlu <gorbadil@192.168.1.5>
2024-07-11 19:42:54 +0300

    LiveClass Started

---
+++ Tailwind/tailFirst/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bubble-gum": '#ff77e9'
+      },
+      padding: {
+        "13": "3.25rem",
+      }
+    },
+  },
+  plugins: [],
+}
+
