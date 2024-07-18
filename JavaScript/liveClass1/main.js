9a3ddef8c8300d427b53861147f68e388e663822
Ersin Mutlu <gorbadil@192.168.1.5>
2024-07-18 15:12:22 +0300

    recap started

---
+++ JavaScript/liveClass1/main.js
@@ -0,0 +1,147 @@
+
+
+// Değişkenler
+// let, const
+let name = "John";
+let age;
+
+let pi = 3.14;
+const surname = "Doe";
+
+age = 21;
+
+// Data Tipleri
+// // primitive ilkel veri tipleri
+// // string, number, boolean, null, undefined
+let address = "Evde oturuyor";
+let address2 = address;
+address = "Dışarı";
+// console.log(address);
+// console.log(address2);
+let number = 21;
+let number2 = "21.5";
+let isStudent = true; // false
+let isNull = null;
+let isUndefined = undefined;
+// console.log(typeof isUndefined);
+
+// // relative (pointer) (referans) veri tipleri
+// // array, object
+let names = ["John", "Jane", "Jack"];
+// console.log(names)
+let names2 = names;
+names.push("Jim");
+// console.log(names2);
+
+let person = {
+  name: "John",
+  age: 21,
+  hobbies: ["Music", "Reading"],
+  address: {
+    city: "Istanbul",
+    country: "Turkey"
+  }
+};
+
+// console.log(person);
+
+// Operatörler
+
+// Aritmetik Operatörler
+// // +, -, *, /, %, ++, --
+// // =, +=, -=, *=, /=, %=
+// let a = 5;
+// let b = 2;
+// let c = "5";
+// let res = a + b
+// let res = a - b
+// let res = a * b
+// let res = a / b
+// let res = a % b
+// let res = ++a
+// let res = a + 1
+// let res = --a
+// let res = a - 1
+
+
+// Karşılaştırma Operatörler
+// // ==, ===, !=, !==, >, <, >=, <=, &&, ||
+
+// let res = a == c
+// let res = a !== c
+// res = a > b
+// res = a < b
+// let res = a >= b
+// let res = a <= b
+
+// && => (ve) her iki koşulda true olmalı onun dışında false
+// || => (veya) her iki koşulda false olmalı onun dışında true
+
+// console.log(res)
+
+
+
+
+// Koşullu 
+
+// // if, else if, else
+// if (koşul) {
+//   // koşul true ise çalışır
+// } else if (koşul) {
+//   // koşul true ise çalışır
+// } else {
+//   // koşul false ise çalışır
+// }
+
+let a = 5;
+let b = 2;
+let c = "hebele";
+
+// if (a === c) {
+//   console.log("a ve c tip bağımlı eşit")
+// } else if (a == c) {
+//   console.log("a ve c tip bağımsız eşit")
+// } else {
+//   console.log("eşit değil")
+// }
+
+// if (a > b) {
+//   let res = "a büyük";
+// } else {
+//   let res = "b büyük";
+// }
+
+// let res = a > b ? "a büyük" : "b büyük";
+
+
+// // switch case
+
+
+
+// Fonksiyonlar
+// Döngüler
+
+
+
+
+
+
+
+
+
+
+// console.log(document.all)
+// console.log(document.all[9].innerHTML)
+
+// let element = document.all[9]
+// element.innerText = "<h1>Hello Canım</h1>"
+// console.log(element)
+
+// document = [html, head, body, h1]
+
+// h1 = {
+//  innerHTML: "Hello World",
+//}
+
+
+// console.log(window)
