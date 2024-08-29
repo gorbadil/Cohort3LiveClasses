03be8c6e32697dde77c4331b7777209af6263dab
Ersin Mutlu <gorbadil@192.168.1.4>
2024-08-29 20:40:49 +0300

    env try

---
+++ React/fetch/src/App.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/users").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  console.log(import.meta.env.VITE_APP_GLOBAL_VARIALBE);
+
+  return (
+    <>
+      <h1>Cohort 3</h1>
+      <div>
+        {data?.users?.map((user, index) => (
+          <div key={index}>
+            <h3>
+              {user.firstName} {user.lastName}{" "}
+            </h3>
+            <p>{user.role}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+// fetch
+// axios =>JS library
+
+// .env => environment variable
+// global variable
+// kodları değiştirmeden değişkenleri değiştirebilmek
+
+// backend => fetch url => değişebilir
+// bu url ben env dosyasına yazıyorum
+
+// variable
+// var {}
+
+// const {}
+// let {}
